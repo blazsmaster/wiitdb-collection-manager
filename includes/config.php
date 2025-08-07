71a77582b69a2098919eb39c529f34b375e3cf3a
blazsmaster <balazsbardocz2006@gmail.com>
2025-08-07 13:31:23 +0200

    Initialize project with basic configuration and homepage

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+    const SITE_TITLE = 'WiiTDB Collection Manager';
+    const SITE_LANG = 'en';
+
+    date_default_timezone_set('UTC');
+
+    error_reporting(E_ALL);
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  require_once 'includes/config.php';
+?>
+
+<!doctype html>
+<html lang='<?php echo SITE_LANG; ?>'>
+  <head>
+    <meta charset='UTF-8'>
+    <meta
+      name='viewport'
+      content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'
+    >
+    <meta
+      http-equiv='X-UA-Compatible'
+      content='ie=edge'
+    >
+    <title><?php echo SITE_TITLE; ?></title>
+  </head>
+  <body>
+    <h2><?php echo SITE_TITLE; ?></h2>
+  </body>
+</html>
